6594492c29deaad3e3cc0f2790fc6386459a0e20
jamwjam <jamwjam@James-Desktop>
2013-10-28 23:09:26 -0400

    Fixed AVLTree put()

--- src/UnitTest/Test.java
+++ src/UnitTest/Test.java
@@ -1,27 +1,44 @@
 package UnitTest;
 
+import org.junit.BeforeClass;
+
 import cmsc420.canonicalsolution.City;
 import cmsc420.canonicalsolution.CityLocationComparator;
 import cmsc420.canonicalsolution.CityNameComparator;
 import cmsc420.sortedmap.AvlGTree;
 import cmsc420.*;
+import junit.framework.Assert;
 import junit.framework.TestCase;
 
 public class Test extends TestCase {
-	public void testAVLG(){
-		City baltimore = new City("D", 10, 5, 10, "Black");
-		City boston = new City("F", 15, 10, 20, "Blue");
-		City olney = new City("C", 14, 10, 21, "Purple");
-		City miami = new City("E", 15, 5, 100, "Red");
-		City anchorage = new City("A", 100, 330, 339, "Yellow");
+	String alphabet;
+	AvlGTree<City, String> avlg;
+	City city;
+	
+    @BeforeClass
+    public void setUp() {
+    	alphabet = "IABMNOPCDEZKLFGH";
+    	
+    	avlg = new AvlGTree<City, String>(new CityNameComparator(), 1);
+    	for (char a : alphabet.toCharArray()){
+    		city = new City(String.valueOf(a), 10, 5, 10, "Black");
+			avlg.put(city, String.valueOf(a));
+		}
+    }
+
+	public void testStressAVLG(){
 		
-		AvlGTree<City, String> avlg = new AvlGTree<City, String>(new CityNameComparator(), 1);
-		avlg.put(baltimore, "B'almore");
-		avlg.put(boston, "boston");
-		avlg.put(miami, "Miami");
-//		avlg.put(olney, "oleney");
-//		avlg.put(anchorage, "anchorage");
-		System.out.println(avlg.toString());
-		System.out.print("yes");;
+	}
+	
+	public void testFirstKey(){
+		assertEquals("A(10.0, 5.0)", avlg.firstKey().toString());
+	}
+	
+	public void testLastKey(){
+		assertEquals("Z(10.0, 5.0)", avlg.lastKey().toString());
+	}
+	
+	public void testSize(){
+		System.out.println(avlg.size());
 	}
 }
--- src/cmsc420/sortedmap/AvlGTree.java
+++ src/cmsc420/sortedmap/AvlGTree.java
@@ -3,10 +3,9 @@ package cmsc420.sortedmap;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.SortedMap;
-
-import cmsc420.sortedmap.AvlTree.AvlNode;
 
 public class AvlGTree <K, V> implements SortedMap<K, V> {
 	private Node root;
@@ -47,96 +46,13 @@ public class AvlGTree <K, V> implements SortedMap<K, V> {
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return root == null ? true : false;
 	}
 
 	@Override
 	public V put(K key, V value) {
 		root = insert(key, value, root);
 		return previousValue;
-	}
-	
-	private Node insert(K key, V value, Node root) {
-		int balanceFactor;
-		if (root == null)
-			root = new Node(key, value);
-		
-		// If key is equal to root.key
-		else if (comparator.compare(root.key, key) == 0){
-			previousValue = root.value;
-			root.value = value;
-		}
-		else {
-			// If key is lesser than root.key
-			if (comparator.compare(root.key, key) > 0)
-				root.left = insert(key, value, root.left);
-			// If key is greater than root.key
-			else if (comparator.compare(root.key, key) < 0)
-				root.right = insert(key, value, root.right);
-			
-			
-			balanceFactor = height(root.left) - height(root.right);
-			
-			if(balanceFactor > g){
-				System.out.println("Weighted left");
-				
-				if(comparator.compare(key, root.left.key) < 0) {
-					System.out.println("rotate right");
-				} else {
-					System.out.println("rotate leftRight");
-				}
-			}
-			else if (balanceFactor < -g){
-				System.out.println("Weighted right");
-				if(comparator.compare(key, root.right.key) > 0) {
-					System.out.println("rotate left");
-					root = rotateLeft(root);
-				} else {
-					System.out.println("rotate rightleft");
-				}
-			}
-		}
-		root.height = max(height(root.left), height(root.right)) + 1;
-		return root;
-	}
-
-	private Node rotateRightLeft(Node root) {
-	    root.left = rotateRight( root.left );
-        return rotateLeft(root);
-	}
-
-	private Node rotateRight(Node root) {
-        Node temp = root.left;
-        root.left = temp.right;
-        temp.right = root;
-        
-        root.height = max( height( root.left ), height( root.right ) ) + 1;
-        temp.height = max( height( temp.left ), root.height ) + 1;
-        return temp;
-	}
-
-	private Node rotateLeftRight(Node root) {
-		root.right = rotateLeft( root.right );
-	    return rotateRight(root);
-	}
-
-	private Node rotateLeft(Node root) {
-		Node temp = root.right;
-	    root.right = temp.left;
-	    temp.left = root;
-	      
-	    root.height = max( height( root.left ), height( root.right ) ) + 1;
-	    temp.height = max( height( temp.right ), root.height ) + 1;
-	    return temp;
-	}
-
-	private int height(Node root){
-		return root == null ? -1 : root.height;
-	}
-	
-	private int max(int left, int right){
-		return left > right ? left: right;
 	}
 	
 	@Override
@@ -152,14 +68,15 @@ public class AvlGTree <K, V> implements SortedMap<K, V> {
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		if (root == null)
+			return 0;
+		else
+			return size(root);
 	}
 
 	@Override
 	public Comparator<? super K> comparator() {
-		// TODO Auto-generated method stub
-		return null;
+		return comparator;
 	}
 
 	@Override
@@ -170,8 +87,18 @@ public class AvlGTree <K, V> implements SortedMap<K, V> {
 
 	@Override
 	public K firstKey() {
-		// TODO Auto-generated method stub
-		return null;
+		if (root == null)
+			throw new NoSuchElementException();
+		else
+			return firstKey(root);
+	}
+
+	@Override
+	public K lastKey() {
+		if (root == null)
+			throw new NoSuchElementException();
+		else
+			return lastKey(root);
 	}
 
 	@Override
@@ -182,12 +109,6 @@ public class AvlGTree <K, V> implements SortedMap<K, V> {
 
 	@Override
 	public Set<K> keySet() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public K lastKey() {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -210,14 +131,133 @@ public class AvlGTree <K, V> implements SortedMap<K, V> {
 		return null;
 	}
 
+	@Override
+	public String toString(){
+		return toString(root, 1);
+	}
+
+	@Override
+	public boolean equals(Object o){
+		return false;
+	}
+
+	private Node insert(K key, V value, Node root) {
+		int balanceFactor;
+		if (root == null)
+			root = new Node(key, value);
+		
+		// If key is equal to root.key
+		else if (comparator.compare(root.key, key) == 0){
+			previousValue = root.value;
+			root.value = value;
+		}
+		else {
+			// If key is lesser than root.key
+			if (comparator.compare(root.key, key) > 0)
+				root.left = insert(key, value, root.left);
+			// If key is greater than root.key
+			else if (comparator.compare(root.key, key) < 0)
+				root.right = insert(key, value, root.right);
+			
+			
+			balanceFactor = height(root.left) - height(root.right);
+			
+			if(balanceFactor > g){
+				if(comparator.compare(key, root.left.key) < 0) {
+					root = rotateRight(root);
+				} else {
+					root = rotateLeftRight(root);
+				}
+			}
+			else if (balanceFactor < -g){
+				if(comparator.compare(key, root.right.key) > 0) {
+					root = rotateLeft(root);
+				} else {
+					root = rotateRightLeft(root);
+				}
+			}
+		}
+		root.height = max(height(root.left), height(root.right)) + 1;
+		return root;
+	}
+
+	private Node rotateRightLeft(Node root) {
+	    root.right = rotateRight( root.right );
+        return rotateLeft(root);
+	}
+
+	private Node rotateRight(Node root) {
+        Node temp = root.left;
+        root.left = temp.right;
+        temp.right = root;
+        
+        root.height = max( height( root.left ), height( root.right ) ) + 1;
+        temp.height = max( height( temp.left ), root.height ) + 1;
+        return temp;
+	}
+
+	private Node rotateLeftRight(Node root) {
+		root.left = rotateLeft( root.left );
+	    return rotateRight(root);
+	}
+
+	private Node rotateLeft(Node root) {
+		Node temp = root.right;
+	    root.right = temp.left;
+	    temp.left = root;
+	      
+	    root.height = max( height( root.left ), height( root.right ) ) + 1;
+	    temp.height = max( height( temp.right ), root.height ) + 1;
+	    return temp;
+	}
+
+	private int height(Node root){
+		return root == null ? -1 : root.height;
+	}
 	
+	private int max(int left, int right){
+		return left > right ? left: right;
+	}
+	
+	private int size(Node root) {
+		if(root == null)
+			return 0;
+		else
+			return 1 + size(root.left) + size(root.right);
+	}
+
+	private K lastKey(Node root) {
+		if (root.right != null)
+			return lastKey(root.right);
+		else
+			return root.key;
+	}
+
+	private K firstKey(Node root) {
+		if(root.left != null)
+			return firstKey(root.left);
+		else
+			return root.key;	
+	}
+
+	public String toString(Node root, int level){
+		if (root == null)
+			return "";
+		else {
+			tab.setLength(0);
+			for (int i = 1; i < level; i++)
+				tab.append("\t");
+	
+			return tab.toString() + root.toString() + "\n" + toString(root.left, level + 1) + toString(root.right, level + 1);
+		}
+	}
+
 	public class Node {
 		private K key;
 		private V value;
         private Node left;
         private Node right;
         private int height;
-		private StringBuffer tab;
 		
 		public Node(K key,V value){
 			this(key, value, null, null);
@@ -231,29 +271,12 @@ public class AvlGTree <K, V> implements SortedMap<K, V> {
 			this.height = 0;
 		}	
 		
+		@Override
 		public String toString(){
 			return String.format("Node<%s, %s>", this.key, this.value);
 		}
 	}
-	
-	
-	public String toString(){
-		return toString(root, 1);
-	}
+
 	StringBuffer tab = new StringBuffer();
 	String left = "left", right = "right", root_dir = "root_dir";
-	public String toString(Node root, int level){
-		if (root == null)
-			return "";
-		else {
-			tab.setLength(0);
-			for (int i = 1; i < level; i++)
-				tab.append("\t");
-
-			return tab.toString() + root.toString() + "\n" + toString(root.left, level + 1) + toString(root.right, level + 1);
-		}
-		
-			
-	}
-	
 }
